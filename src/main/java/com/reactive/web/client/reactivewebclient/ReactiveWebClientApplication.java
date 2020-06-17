0e369575057e9ce2163e745ea51c89871948d58c
rishabhgupta18 <rishabhgupta.cool86@gmail.com>
2020-06-17 07:36:43 +0530

    initial setup

---
+++ src/main/java/com/reactive/web/client/reactivewebclient/ReactiveWebClientApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.web.client.reactivewebclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveWebClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveWebClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reactive/web/client/reactivewebclient/ReactiveWebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.web.client.reactivewebclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveWebClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
